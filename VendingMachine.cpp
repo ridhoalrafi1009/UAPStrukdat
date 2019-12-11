ab7ddb32d3ff5b33dc00f43078b02e48daa5f99f
sucihikmawati <48645514+sucihikmawati@users.noreply.github.com>
2019-12-11 09:36:15 +0700

    menu pilihan

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<conio.h>
+#include "define.h"
+#include "rumus.h"
 
+using namespace std;
+
+
+int mulai() {
+	int pil,i;
+	string baru;
+	tumpuk.atas=-1;
+	
+	cetak<<"===============VENDING MACHINE =================\n"<<endl;
+	
+	lakukan {
+		cetak<<"Hal yang bisa anda lakukan :\n";
+		cetak<<"1.Memasukkan Minuman\n";
+		cetak<<"2.Membeli Minuman\n";
+		cetak<<"3.Print Data\n";
+		cetak<<"4.End Data\n";
+		cetak<<"\nPilihan = ";
+		input>>pil;
